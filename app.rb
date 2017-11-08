9d9a76476db9d4c167fe351b3fa2ce8d5a1f7a28
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-08 17:57:01 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,60 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require './models'
+
+# Database configuration
+set :database, "sqlite3:development.sqlite3"
+
+# messages index route
+get '/' do
+  @messages = Message.all
+  erb :index
+end
+
+#messages edit route
+post '/messages' do
+  message = Message.create(
+    title: params[:title],
+    body: params[:body]
+  )
+
+  redirect "/messages/#{message.id}"
+end
+
+#messages new route
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+patch '/messages/:id' do
+  message = Message.find_by_id(params[:id])
+  message.update(
+    title: params[:title],
+    body: params[:body]
+  )
+
+  redirect "/messages/#{message.id}"
+end
+
+
+#messages show route
+get '/messages/:id' do
+  @message = Message.find_by_id(params[:id])
+  erb :'messages/show'
+end
+
+get '/messages/:id/edit' do
+  
+end
+
+
+# Providing model information to the view
+# requires an instance variable (prefixing with the '@' symbol)
+
+# Example 'User' index route
+
+# get '/users' do
+#   @users = User.all
+#   erb :users
+# end
---
+++ models.rb
@@ -0,0 +1,10 @@
+# Models
+
+# ActiveRecord classes go here. These are object-model
+# representations of your database tables. All classes defined
+# here must inherit from ActiveRecord::Base
+
+# e.g.
+class Message < ActiveRecord::Base
+
+end
